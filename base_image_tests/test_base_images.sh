1d3ab7019398f85121c50e731b736058a37dbeab
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2020-09-23 11:09:25 +0200

    Add log output to file and Amazon Linux

--- base_image_tests/test_base_images.sh
+++ base_image_tests/test_base_images.sh
@@ -11,9 +11,17 @@
 # https://www.centos.org/centos-linux/
 #seq 6 8 | xargs -I {} bash -c 'singularity pull docker://centos:{} || echo {} failed'
 
+# https://aws.amazon.com/de/amazon-linux-ami/
+# https://aws.amazon.com/de/amazon-linux-2/
+# https://hub.docker.com/_/amazonlinux
+#seq 1 2 | xargs -I {} bash -c 'singularity pull docker://amazonlinux:{} || echo {} failed'
+
 # Test job submission.
+
+MACHINE=`basename $(readlink scheduler-bindings.conf) | cut -d "." -f 1`
 
 ls *.sif | sort -V | xargs -I {} \
 bash -c '{ SINGULARITYENV_USER=$USER ./bind_scheduler.sh singularity exec --cleanenv {} ./fake_workflow_slurm.sh; \
-echo -----------------------------------------------; }'
+echo -----------------------------------------------; }' \
+> ${MACHINE}.log 2>&1
 
