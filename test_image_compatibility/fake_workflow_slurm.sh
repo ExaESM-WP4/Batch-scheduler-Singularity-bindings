f0d3cfec58adffc13268a78bf790185c2cb95a70
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2020-09-23 11:50:25 +0200

    Add description

--- test_image_compatibility/fake_workflow_slurm.sh
+++ test_image_compatibility/fake_workflow_slurm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Tests the following SLURM commands
+# sinfo, sbatch, squeue, scancel
+
 sinfo 1> /dev/null && echo successful call: sinfo || echo failed to call: sinfo
 function sleep_then_kill { squeue -u $USER; scancel "$1"; sleep 5; squeue -u $USER; }
 sleep_then_kill $( sbatch test_job_slurm.sh | awk '{print $4}' )
